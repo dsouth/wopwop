fe2559ec5601e19615d52db46ecfaad31f0f3097
Doug South <doug.south@gmail.com>
2017-04-30 16:23:31 +1000

    geometry from lesson 8 lazyfoo

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,9 @@ const int SCREEN_HEIGHT = 768;
 bool init();
 bool loadMedia();
 void close();
-SDL_Texture* loadTexture(const char* path);
 
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
-SDL_Texture* gTexture = NULL;
 
 bool init() {
   bool success = false;
@@ -36,13 +34,7 @@ bool init() {
         success = false;
       } else {
         SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        int imgFlags = IMG_INIT_PNG;
-        if(!(IMG_Init(imgFlags) & imgFlags)) {
-          printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-          success = false;
-        } else {
-          success = true;
-        }
+        success = true;
       }
     }
   }
@@ -50,8 +42,6 @@ bool init() {
 }
 
 void close() {
-  SDL_DestroyTexture(gTexture);
-  gTexture = NULL;
   SDL_DestroyRenderer(gRenderer);
   SDL_DestroyWindow(gWindow);
   gWindow = NULL;
@@ -60,28 +50,8 @@ void close() {
   SDL_Quit();
 }
 
-SDL_Texture* loadTexture(const char* path) {
-  SDL_Texture* newTexture = NULL;
-  SDL_Surface* loadedSurface = IMG_Load(path);
-  if (loadedSurface == NULL) {
-    printf("Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError());
-  } else {
-    newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-    if (newTexture == NULL) {
-      printf("Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError());
-    }
-    SDL_FreeSurface(loadedSurface);
-  }
-  return newTexture;
-}
-
 bool loadMedia() {
   bool success = true;
-  gTexture = loadTexture("../res/elements.png");
-  if (gTexture == NULL) {
-    printf("Failed to load texture image!\n");
-    success = false;
-  }
   return success;
 }
 
@@ -104,8 +74,27 @@ int main(int argc, char* args[]) {
         quit = true;
       }
 
+      SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00,  0x00, 0xFF);
       SDL_RenderClear(gRenderer);
-      SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+      SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,
+                           SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+      SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+      SDL_RenderFillRect(gRenderer, &fillRect);
+
+      SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6,
+                              SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
+      SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+      SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+      SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+      SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+      SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+      for(int i = 0; i < SCREEN_HEIGHT; i += 4) {
+        SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+      }
+
       SDL_RenderPresent(gRenderer);
     }
   }
