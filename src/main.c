4636f4b0f6e7c7c41c9c053049b4f50e54044d67
Doug South <doug.south@gmail.com>
2017-05-07 20:16:30 +1000

    lazy foo lesson 14 animation and vsync

--- src/main.c
+++ src/main.c
@@ -14,8 +14,9 @@ void close();
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
-texture* gModulatedTexture;
-texture* gBackgroundTexture;
+const int WALKING_ANIMATION_FRAMES = 4;
+SDL_Rect gSpriteClips[WALKING_ANIMATION_FRAMES];
+texture* gSpriteSheetTexture;
 
 int init() {
   int success = 0;
@@ -33,7 +34,7 @@ int init() {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
       success = 0;
     } else {
-      gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+      gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
       if (gRenderer == NULL) {
         printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
         success = 0;
@@ -43,14 +44,12 @@ int init() {
       }
     }
   }
-  gModulatedTexture = init_texture();
-  gBackgroundTexture = init_texture();
+  gSpriteSheetTexture = init_texture();
   return success;
 }
 
 void close() {
-  freeTexture(gModulatedTexture);
-  freeTexture(gBackgroundTexture);
+  freeTexture(gSpriteSheetTexture);
   SDL_DestroyRenderer(gRenderer);
   SDL_DestroyWindow(gWindow);
   gWindow = NULL;
@@ -61,15 +60,29 @@ void close() {
 
 int loadMedia() {
   int success = 1;
-  if(!loadTextureFromFile(gModulatedTexture, gRenderer, "res/fadeout.png")){
-    printf("Failed to load front texture!\n");
+  if(!loadTextureFromFile(gSpriteSheetTexture, gRenderer, "res/foo.png")){
+    printf("Failed to load walking animation texture!\n");
     success = 0;
   } else {
-    setBlendModeTexture(gModulatedTexture, SDL_BLENDMODE_BLEND);
-  }
-  if(!loadTextureFromFile(gBackgroundTexture, gRenderer, "res/fadein.png")) {
-    printf("Failed to load background texture!\n");
-    success = 0;
+    gSpriteClips[0].x = 0;
+    gSpriteClips[0].y = 0;
+    gSpriteClips[0].w = 64;
+    gSpriteClips[0].h = 205;
+
+    gSpriteClips[1].x = 64;
+    gSpriteClips[1].y = 0;
+    gSpriteClips[1].w = 64;
+    gSpriteClips[1].h = 205;
+
+    gSpriteClips[2].x = 128;
+    gSpriteClips[2].y = 0;
+    gSpriteClips[2].w = 64;
+    gSpriteClips[2].h = 205;
+
+    gSpriteClips[3].x = 196;
+    gSpriteClips[3].y = 0;
+    gSpriteClips[3].w = 64;
+    gSpriteClips[3].h = 205;
   }
   return success;
 }
@@ -85,34 +98,30 @@ int main(int argc, char* args[]) {
 
   int quit = 0;
   SDL_Event e;
-  Uint8 a = 255;
+  int frame = 0;
+  const int frame_interval = 5;
 
   while (!quit) {
     while (SDL_PollEvent(&e) != 0) {
       if (e.type == SDL_QUIT) {
         quit = 1;
-      } else if (e.type == SDL_KEYDOWN) {
-        if (e.key.keysym.sym == SDLK_w) {
-          if (a + 32 > 255) {
-            a = 255;
-          } else {
-            a += 32;
-          }
-        } else if (e.key.keysym.sym == SDLK_s) {
-          if (a - 32 < 0) {
-            a = 0;
-          } else {
-            a -= 32;
-          }
-        }
-      }
-        
-      SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-      SDL_RenderClear(gRenderer);
-      renderTexture(gBackgroundTexture, gRenderer, 0, 0, NULL);
-      setAlphaTexture(gModulatedTexture, a);
-      renderTexture(gModulatedTexture, gRenderer, 0, 0, NULL);
-      SDL_RenderPresent(gRenderer);
+      } 
+    }        
+    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(gRenderer);
+    
+    SDL_Rect* currentClip = &gSpriteClips[frame / frame_interval];
+    renderTexture(gSpriteSheetTexture, 
+                  gRenderer, 
+                  (SCREEN_WIDTH - currentClip->w) / 2, 
+                  (SCREEN_HEIGHT - currentClip->h) / 2,
+                  currentClip);
+    SDL_RenderPresent(gRenderer);
+    
+    frame++;
+    if(frame / frame_interval >= WALKING_ANIMATION_FRAMES) {
+      frame = 0;
+      
     }
   }
   close();
